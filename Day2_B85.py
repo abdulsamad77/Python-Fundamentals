64536936e377fa279891483a22e8c07efc52903a
Abdul Samad <151526781+abdulsamad77@users.noreply.github.com>
2024-03-01 01:15:25 +0530

    Add files via upload

---
+++ Day2_B85.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+variables in python;
+
+
+# In[1]:
+
+
+x=2
+y=3
+z=x+y
+print(z);
+
+
+# In[ ]:
+
+
+Rules for declaring the variable in python;
+
+
+# In[ ]:
+
+
+1 : no space between variables while declaring them
+
+
+# In[2]:
+
+
+first name='samad'
+print(first name)
+
+
+# In[3]:
+
+
+first_name='samad'
+print(first_name)
+
+
+# In[4]:
+
+
+firstname='samad'
+print(firstname)
+
+
+# In[ ]:
+
+
+2 : variable name should not start with a number
+
+
+# In[6]:
+
+
+1name='abdul'
+print(1name)
+
+
+# In[7]:
+
+
+name1='abdul'
+print(name1)
+
+
+# In[ ]:
+
+
+3 : variable should not contain any special character
+
+
+# In[ ]:
+
+
+@ $ % ^ &
+
+
+# In[8]:
+
+
+name@='abdul'
+print(name@)
+
+
+# In[ ]:
+
+
+Introduction to datatypes in python;
+
+
+# In[ ]:
+
+
+1 : string
+2 : number == integers and floats (0.01)
+3 : list
+4 : tuples
+5 : dictionary
+
+
+# In[ ]:
+
+
+Classification of Datatypes
+
+
+# In[ ]:
+
+
+they are classified in two categories
+1 : mutables datatypes == which can be able to edit and alter ==> flexible in nature
+2 : immutable datatypes == which can not be able to edit and alter ==> fixed
+
+
+# In[ ]:
+
+
+Introduction to string datatype ;
+
+
+# In[ ]:
+
+
+Defination : a string is a series of characters declared in quotes
+classification : it is classified as an immutable datatype (fixed)
+
+
+# In[ ]:
+
+
+how to define the string datatype
+ans : by using single quote '' or double quote ""
+
+
+# In[9]:
+
+
+name= "samad"
+print(name)
+
+
+# In[10]:
+
+
+name1='abdul'
+print(name1)
+
+
+# In[11]:
+
+
+fullname= 'abdul samad'
+print(fullname)
+
+
+# In[12]:
+
+
+print(fullname.title())
+
+
+# In[ ]:
+
+
+
+
