927fa64cf5772994abd46fa22b58af2622bc5717
Abdul Samad <151526781+abdulsamad77@users.noreply.github.com>
2024-03-07 19:21:28 +0530

    Add files via upload

---
+++ Day6_B85.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+introduction to looping statement :
+
+
+# In[ ]:
+
+
+1. for loop
+2. while loop
+
+
+# In[1]:
+
+
+students=['naveen','meghna','khadija','ramesh','naveed','shafia']
+print(students)
+
+
+# In[3]:
+
+
+message= f'keep up the good work {students[0]}'
+print(message)
+
+
+# In[4]:
+
+
+message= f'keep up the good work {students[1]}'
+print(message)
+
+
+# In[5]:
+
+
+message= f'keep up the good work {students[2]}'
+print(message)
+
+
+# In[ ]:
+
+
+# general syntax of FOR LOOP :
+
+
+# In[ ]:
+
+
+for tempvar in mainvar:
+    print(tempvar)
+
+
+# In[7]:
+
+
+for x in students:
+    print(f"keep up the good work, {x}")
+
+
+# In[8]:
+
+
+for tempvar in mainvar:
+print(tempvar)
+
+
+# In[10]:
+
+
+for a in students:
+    print(a)
+
+
+# In[12]:
+
+
+for b in students:
+    print(b)
+
+
+# In[14]:
+
+
+for ironman in students:
+    print(ironman)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
